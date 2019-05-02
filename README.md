c44d75b95da18549b93c8026f9cbe15620162a4d
Jonathan Dayton <jonathandayton23@gmail.com>
2019-05-02 11:35:52 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Express example
+
+Example express app for Louisa
+
+## Quick start
+
+Make sure you have npm and node installed, then run
+
+```bash
+npm install
+```
+
+To run the server:
+
+```bash
+node index.js
+```
+
+And go to http://localhost:3000 in your browser.
---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.use('/', express.static(path.join(__dirname, 'static')))
+app.get('/coolstuff', (req, res) => res.send("This is a cool custom response"))
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
